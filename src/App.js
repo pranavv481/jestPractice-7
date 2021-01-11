27a29102d4cca8fd5374d829d0d3043695041b06
pranavv481 <pranavv481@gmail.com>
2021-01-11 13:04:20 +0530

    redux component

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import {connect} from 'react-redux';
+import React, {Component} from 'react';
+import Congrats from "./Congrats";
+import GuessedWords from "./GuessedWords";
+import {getSecretWord} from './actions';
+import Input from './input';
+export class UnconnectedApp extends Component {
+  componentDidMount(){
+      // get the secret word
+      this.props.getSecretWord();
+  }
+  render(){
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <div>the secret word is {this.props.secretWord}</div>
+       <Congrats success={this.props.success}/>
+       <Input/>
+       <GuessedWords guessedWords={this.props.guessedWords}/>
+      </div>
+    );
+  }
+ 
+}
+
+const mapStateToProps = (state) =>{
+    const {success, guessedWords, secretWord } = state
+    return {success, guessedWords, secretWord}
+}
+
+export default connect(mapStateToProps,{getSecretWord})(UnconnectedApp);
